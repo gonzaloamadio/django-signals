1792f0f44dc5f5cb532636c7d92b412e116a9249
Gonzalo Amadio <gonzaloamadio@gmail.com>
2018-11-02 19:32:26 +1100

    Deleted pyc

--- signals/signals/urls.py
+++ signals/signals/urls.py
@@ -11,6 +11,7 @@ router = routers.DefaultRouter()
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api/v1/', include('signals.apps.entities_2.api_v1.urls',namespace='v1')),
+#    url(r'^api/v1/', include('signals.apps.entities_2.api_v1.urls',namespace='v1')),
+    url(r'^api/v1/', include('signals.apps.entities.api_v1.urls',namespace='v1')),
     url(r'^admin/', admin.site.urls),
 ]
